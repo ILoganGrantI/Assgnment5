ad91b1be1ab2c3f467ff907b363efda3ca7749ce
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-24 16:47:18 +0000

    --

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let diameter = 0
 let area = 0
 let circumference = 0
+let radius = 0
+let result = 0
+let answer = 0
 
 document.getElementById('button').addEventListener('click', funk)
 
 function funk () {
-	diameter = document.getElementById('D').Value
-	diameter = parseInt('D')
+	diameter = document.getElementById('D').value
+	diameter = parseInt(diameter)
   circumference = diameter * Math.PI
 	document.getElementById('C-1').innerHTML = circumference
+  radius = diameter / 2
+  result = radius * radius
+  answer = Math.PI * result
+	document.getElementById('A-1').innerHTML = answer
+
 }
